b11b37d01bf56455b25041ef68090c0f4df7969a
Rosa <117862247+rosahadi@users.noreply.github.com>
2023-07-04 14:41:01 +0300

    Add 'use strict' directive

--- src/js/game.js
+++ src/js/game.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
